68048258f17a5d144b2e4841fd6b1ba429803ed2
Jan Hančič <jan.hancic@gmail.com>
2013-10-16 20:27:03 +0100

    2. exercise (scopes)

--- exercises/tricky-scopes/main.js
+++ exercises/tricky-scopes/main.js
@@ -1,16 +1,21 @@
 var app = angular.module("exercise",[]);
 
-app.controller("main",function($scope) {
-  $scope.count = 0
-})
+app.controller( "main", function ( $scope ) {
+	$scope.data = {
+		count: 0
+	};
+} );
 
 app.controller("child",function($scope) {
-  $scope.increment = function() {
-    $scope.count += 1
-  }
-  $scope.delayedIncrement = function() {
-    setTimeout(function() {
-      $scope.count += 1
-    },250); // just perceptible delay
-  }
+	$scope.increment = function() {
+		$scope.data.count += 1
+	}
+
+	$scope.delayedIncrement = function() {
+		setTimeout( function () {
+			$scope.$apply(function() {
+				$scope.data.count += 1
+			} );
+		}, 250 ); // just perceptible delay
+	}
 })
